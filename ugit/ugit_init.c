6b6be6c3a42399852e6087b392cf23a7fb03ed1f
g0ldenjays <ivanjays2005@gmail.com>
2025-09-24 13:19:17 -0300

    (Iván Gallardo) ugit_init.c para CMD

--- ugit/ugit_init.c
+++ ugit/ugit_init.c
@@ -1,15 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include "ugit.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void crear_versions() {
-
-	//Si carpeta existe, copiar, sino, crearla y luego copiar
-	if ( system( "mkdir versions" ) == 0 ) {
-		printf("Carpeta 'versions' creada exitosamente. \n");
-	} else {
-		printf("La carpeta 'versions' ya existe. \n");
-	}
-
-
+void cmd_init() {
+    if (system("mkdir versions") == 0) {
+        printf("Carpeta 'versions' creada exitosamente.\n");
+    } else {
+        printf("La carpeta 'versions' ya existe.\n");
+    }
 }
+
+
