9a0903b0ef0ba5b89e0ff2d3094991a0b525f151
AndrewhiteCode <bardok3786@gmail.com>
2025-09-24 13:16:01 -0300

    Andres Barbosa
    Actualizacion de parsing se comenta futuras funciones

--- ugit/ugit_parsing.c
+++ ugit/ugit_parsing.c
@@ -1,53 +1,26 @@
-#include <stdio.h>
+#include "ugit.h"
 #include <string.h>
+#include <stdio.h>
 
-void cmd_init() {
-    printf("[ugit] Inicializando repositorio...\n");
-    // TODO: crear carpeta .ugit
-}
-
-void cmd_add(const char *archivo) {
-    printf("[ugit] Agregando archivo: %s\n", archivo);
-    // TODO: guardar referencia del archivo
-}
-
-void cmd_commit() {
-    printf("[ugit] Creando commit...\n");
-    // TODO: copiar archivos agregados a carpeta de commits
-}
-
-void cmd_version() {
-    printf("[ugit] Listando versiones...\n");
-    // TODO: mostrar archivos de la carpeta commits
-}
-
-int main(int argc, char *argv[]) {
+void parse_command(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Uso: ugit <comando> [opciones]\n");
-        return 1;
+        printf("Uso: ugit <comando>\n");
+        return;
     }
 
     if (strcmp(argv[1], "init") == 0) {
         cmd_init();
-    }
-    else if (strcmp(argv[1], "add") == 0) {
-        if (argc < 3) {
-            printf("Uso: ugit add <archivo>\n");
-            return 1;
-        }
-        cmd_add(argv[2]);
-    }
-    else if (strcmp(argv[1], "commit") == 0) {
-        cmd_commit();
-    }
-    else if (strcmp(argv[1], "version") == 0) {
+   /* } else if (strcmp(argv[1], "version") == 0) {
         cmd_version();
-    }
-    else {
+    } else if (strcmp(argv[1], "add") == 0) {
+        if (argc < 3) {
+            printf("Falta archivo a añadir\n");
+        } else {
+            cmd_add(argv[2]);
+        }
+    } else if (strcmp(argv[1], "commit") == 0) {
+        cmd_commit();*/
+    } else {
         printf("Comando desconocido: %s\n", argv[1]);
-        printf("Comandos disponibles: init, add, commit, version\n");
     }
-
-    return 0;
 }
-
