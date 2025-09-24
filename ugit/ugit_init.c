0573c2f1920e598d1ae771cd043e00e91decdda7
g0ldenjays <ivanjays2005@gmail.com>
2025-09-24 12:25:29 -0300

    Iván Gallardo

---
+++ ugit/ugit_init.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void crear_versions();
+
+int main() {
+
+	crear_versions();
+	return 0;
+}
+
+void crear_versions() {
+
+	//Si carpeta existe, copiar, sino, crearla y luego copiar
+	if ( system( "mkdir versions" ) == 0 ) {
+		printf("Carpeta 'versions' creada exitosamente. \n");
+	} else {
+		printf("La carpeta 'versions' ya existe. \n");
+	}
+
+}
