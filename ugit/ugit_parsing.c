d622cd89d64196d37e74131e8376a262ef52f01a
AndrewhiteCode <bardok3786@gmail.com>
2025-09-28 00:21:13 -0300

    Update ugit_parsing.c
    Se quitan los comentario de las futuras funcionalidades ya que se implementan a partir de ahora

--- ugit/ugit_parsing.c
+++ ugit/ugit_parsing.c
@@ -3,24 +3,20 @@
 #include <stdio.h>
 
 void parse_command(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Uso: ugit <comando>\n");
-        return;
-    }
-
     if (strcmp(argv[1], "init") == 0) {
         cmd_init();
-   /* } else if (strcmp(argv[1], "version") == 0) {
+    }
+    else if (strcmp(argv[1], "add") == 0) {
+        cmd_add();
+    }
+    else if (strcmp(argv[1], "commit") == 0) {
+        cmd_commit();
+    }
+    else if (strcmp(argv[1], "version") == 0) {
         cmd_version();
-    } else if (strcmp(argv[1], "add") == 0) {
-        if (argc < 3) {
-            printf("Falta archivo a añadir\n");
-        } else {
-            cmd_add(argv[2]);
-        }
-    } else if (strcmp(argv[1], "commit") == 0) {
-        cmd_commit();*/
-    } else {
+    }
+    else {
         printf("Comando desconocido: %s\n", argv[1]);
+        printf("Comandos disponibles: init, add, commit, version\n");
     }
 }
