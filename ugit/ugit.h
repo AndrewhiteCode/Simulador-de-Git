d95cffafee917c7a8b5375278cb666e6d2e8c717
AndrewhiteCode <bardok3786@gmail.com>
2025-09-28 00:47:35 -0300

    Ugit.h

---
+++ ugit/ugit.h
@@ -0,0 +1,13 @@
+#ifndef UGIT_H
+#define UGIT_H
+
+// Declaraciones de funciones
+void cmd_init();
+void cmd_add();
+void cmd_commit();
+void cmd_version();
+void parse_command(int argc, char *argv[]);
+
+#endif
+
+
