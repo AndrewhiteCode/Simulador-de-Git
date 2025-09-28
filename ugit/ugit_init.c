0ca460d155346e84135951e0bbe8f49c68c0ca6d
AndrewhiteCode <bardok3786@gmail.com>
2025-09-28 00:19:54 -0300

    Update de ugit_init.c
    Se opta por separar la creación de direcciones en el "init" ya que se hacen directamente en la función que se ejecuta

--- ugit/ugit_init.c
+++ ugit/ugit_init.c
@@ -4,21 +4,9 @@
 #include <string.h>
 
 void cmd_init() {
-	if (system("mkdir .ugit") == 0) {
-		printf("Carpeta '.ugit' creada exitosamente.\n");
-
-		if (system("touch .ugit/add.txt") == 0) {
-			printf("Archivo 'add.txt' creado exitosamente.\n");
-		} else {
-			printf("Error al crear el archivo 'add.txt'.\n");
-		}
-
-		if (system("mkdir .ugit/versions") == 0) {
-			printf("Carpeta '.ugit/versions' creada exitosamente.\n");
-		} else {
-			printf("Error al crear la carpeta '.ugit/versions'.\n");
-		}
-	} else {
-		printf("La carpeta '.ugit' ya existe.\n");
-	}
+    if (system("mkdir ugit") == 0) {
+        printf("Carpeta 'ugit' creada exitosamente.\n");
+    } else {
+        printf("La carpeta 'ugit' ya existe.\n");
+    }
 }
